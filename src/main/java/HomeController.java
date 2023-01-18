ba783c848bb8140906982517033eadc4220324f6
Brittney Brinson <brittneyc95@outlook.com>
2023-01-18 10:23:31 -0500

    Working on Controllers in Spring

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,13 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class HomeController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String landingPage(){
+        return "This is the landing page!";
+    }
+}
--- src/main/java/com/codeup/springwork/SpringWorkApplication.java
+++ src/main/java/com/codeup/springwork/SpringWorkApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWorkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringWorkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWorkApplication.class, args);
+    }
 
 }

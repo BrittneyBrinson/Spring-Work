f193527830c8b88257713e53ecc71b299bd3e981
Brittney Brinson <brittneyc95@outlook.com>
2023-01-18 14:55:16 -0500

    Finished the Math Controller exercise in Spring

--- src/main/java/com/codeup/springwork/HomeController.java
+++ src/main/java/com/codeup/springwork/HomeController.java
@@ -1,3 +1,5 @@
+package com.codeup.springwork;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;

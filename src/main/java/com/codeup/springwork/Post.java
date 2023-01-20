ad0fa26df13e7d2e6b3418c8b1bfd1a0c40d2b32
Brittney Brinson <brittneyc95@outlook.com>
2023-01-20 17:08:05 -0500

    Almost done

--- src/main/java/com/codeup/springwork/Post.java
+++ src/main/java/com/codeup/springwork/Post.java
@@ -18,9 +18,16 @@ public class Post {
     @Column(nullable = false)
     private String body;
 
-    public Post() {
+
+
+    public Post(PostRepository postRepo) {
         this.title = title;
         this.body = body;
+
+    }
+
+    public Post() {
+
     }
 
     public String getTitle() {
@@ -39,29 +46,5 @@ public class Post {
         this.body = body;
     }
 
-    @GetMapping("/show")
-    public String showPost(Model model){
-        Post newPost = new Post();
-        newPost.setTitle("Anyways, life is great");
-        newPost.setBody("Great things are happening for me");
-        model.addAttribute("title",newPost.getTitle());
-        model.addAttribute("body", newPost.getBody());
-        return "posts/show";
-    }
 
-    @GetMapping("/showAll")
-    public String showAllPosts(Model model){
-        Post postOne = new Post();
-        postOne.setTitle("I'm going to Denver next week");
-        postOne.setBody("It's going to be soo much fun");
-        Post postTwo = new Post();
-        postTwo.setTitle("Mental Health Matters");
-        postTwo.setBody("Always take care of your mental and physical health");
-        ArrayList <Post> postList = new ArrayList<Post>();
-        postList.add(postOne);
-        postList.add(postTwo);
-        model.addAttribute("title", postList);
-
-        return "posts/index";
-    }
 }
--- src/main/java/com/codeup/springwork/controllers/PostController.java
+++ src/main/java/com/codeup/springwork/controllers/PostController.java
@@ -1,13 +1,52 @@
 package com.codeup.springwork.controllers;
 
+import com.codeup.springwork.Post;
+import com.codeup.springwork.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
-//    @GetMapping("/posts")
-//    @ResponseBody
+private final PostRepository postRepo;
+
+    public PostController(PostRepository postRepo) {
+        this.postRepo = postRepo;
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(){
+        return "posts/create";
+    }
+
+//    @GetMapping("/show")
+//    public String showPost(Model model){
+//        Post newPost = new Post(postRepo);
+//        newPost.setTitle("Anyways, life is great");
+//        newPost.setBody("Great things are happening for me");
+//        model.addAttribute("title",newPost.getTitle());
+//        model.addAttribute("body", newPost.getBody());
+//        return "posts/show";
+//    }
+
+    @GetMapping("/showAll")
+    public String showAllPosts(Model model){
+        Post postOne = new Post(postRepo);
+        postOne.setTitle("I'm going to Denver next week");
+        postOne.setBody("It's going to be soo much fun");
+        Post postTwo = new Post(postRepo);
+        postTwo.setTitle("Mental Health Matters");
+        postTwo.setBody("Always take care of your mental and physical health");
+        ArrayList<Post> postList = new ArrayList<Post>();
+        postList.add(postOne);
+        postList.add(postTwo);
+        model.addAttribute("title", postList);
+
+        return "posts/index";
+    }
 
 
 }
